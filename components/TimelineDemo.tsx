83b36a54706c4203b660cdd763c20f26c56f5f84
Leandro <leandro.soares.eneterio@gmail.com>
2025-04-20 20:20:17 -0300

    version: 1.0

--- components/TimelineDemo.tsx
+++ components/TimelineDemo.tsx
@@ -16,21 +16,21 @@ export function TimelineDemo() {
                     </p>
                     <div className="grid grid-cols-2 gap-4">
                         <img
-                            src='/placeholder.svg'
+                            src='/timeline/salaufpe.jpg'
                             alt="startup template"
                             width={500}
                             height={500}
                             className="h-20 w-full rounded-lg object-cover shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] md:h-44 lg:h-60"
                         />
                         <img
-                            src='/placeholder.svg'
+                            src='/timeline/paredeufpe.jpg'
                             alt="startup template"
                             width={500}
                             height={500}
                             className="h-20 w-full rounded-lg object-cover shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] md:h-44 lg:h-60"
                         />
                         <img
-                            src='/placeholder.svg'
+                            src='/timeline/campusufpe.jpg'
                             alt="startup template"
                             width={500}
                             height={500}
@@ -52,28 +52,28 @@ export function TimelineDemo() {
                     </p>
                     <div className="grid grid-cols-2 gap-4">
                         <img
-                            src='/placeholder.svg'
+                            src='/timeline/analisedados2.png'
                             alt="hero template"
                             width={500}
                             height={500}
                             className="h-20 w-full rounded-lg object-cover shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] md:h-44 lg:h-60"
                         />
                         <img
-                            src='/placeholder.svg'
+                            src='/timeline/analisedados3.png'
                             alt="feature template"
                             width={500}
                             height={500}
                             className="h-20 w-full rounded-lg object-cover shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] md:h-44 lg:h-60"
                         />
                         <img
-                            src='/placeholder.svg'
+                            src='/timeline/analisedados4.png'
                             alt="bento template"
                             width={500}
                             height={500}
                             className="h-20 w-full rounded-lg object-cover shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] md:h-44 lg:h-60"
                         />
                         <img
-                            src='/placeholder.svg'
+                            src='/timeline/portfolio1.png'
                             alt="cards template"
                             width={500}
                             height={500}
@@ -97,14 +97,14 @@ export function TimelineDemo() {
                                     designation: "Plataforma global de freelancers onde ofereço serviços de dados e SEO desde 2023.",
                                 }}
                             >
-                                    <a
-                                        href="https://fiverr.com"
-                                        target="_blank"
-                                        rel="noopener noreferrer"
-                                        className="font-bold bg-clip-text text-transparent bg-gradient-to-br from-green-300 to-green-500 text-xl md:text-2xl max-w-full"
-                                    >
-                                        Fiverr
-                                    </a>
+                                <a
+                                    href="https://fiverr.com"
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                    className="font-bold bg-clip-text text-transparent bg-gradient-to-br from-green-300 to-green-500 text-xl md:text-2xl max-w-full"
+                                >
+                                    Fiverr
+                                </a>
                             </AnimatedTooltip>{" "}
                         </div>
                         mas, infelizmente, nenhum na área que eu desejava. A grande maioria sendo trabalhos de escrita de artigos e aperfeiçoamento do SEO.
@@ -116,63 +116,28 @@ export function TimelineDemo() {
                     </p>
                     <div className="grid grid-cols-2 gap-4">
                         <img
-                            src='/placeholder.svg'
+                            src='/timeline/analisedadosimportante.png'
                             alt="hero template"
                             width={500}
                             height={500}
                             className="h-20 w-full rounded-lg object-cover shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] md:h-44 lg:h-60"
                         />
                         <img
-                            src='/placeholder.svg'
+                            src='/timeline/sqlserver2.png'
                             alt="feature template"
                             width={500}
                             height={500}
                             className="h-20 w-full rounded-lg object-cover shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] md:h-44 lg:h-60"
                         />
                         <img
-                            src='/placeholder.svg'
+                            src='/timeline/webgallery.png'
                             width={500}
                             height={500}
                             className="h-20 w-full rounded-lg object-cover shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] md:h-44 lg:h-60"
                         />
                         <img
-                            src='/placeholder.svg'
+                            src='/timeline/analiseimg2.png'
                             alt="cards template"
-                            width={500}
-                            height={500}
-                            className="h-20 w-full rounded-lg object-cover shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] md:h-44 lg:h-60"
-                        />
-                    </div>
-                </div>
-            ),
-        },
-        {
-            title: "início de 2023",
-            content: (
-                <div >
-                    <p className="mb-8 text-xs font-normal md:text-sm bg-gradient-to-r from-purple-400 to-pink-400 bg-clip-text text-transparent">
-                        Aprendi novas ferramentas de desenvolvimento como <span className="italic">NextJS</span>, <span className="italic">React</span> e <span className="italic">Tailwind CSS</span>. Também busquei conhecimento sobre SEO para melhor ranking do site.
-                    </p>
-                    <p className="mb-8 text-xs font-normal md:text-sm bg-gradient-to-r from-purple-400 to-pink-400 bg-clip-text text-transparent">
-                        Realizei projetos de analise de dados e de desenvolvimento de web Aplications. Você pode dar uma olhada na barra superior à página na opção &quot;Projetos&quot;. Começei a usar UI das plataformar aceternety e shadcn.
-                    </p>
-                    <div className="grid grid-cols-2 gap-4">
-                        <img
-                            src='/placeholder.svg'
-                            alt="hero template"
-                            width={500}
-                            height={500}
-                            className="h-20 w-full rounded-lg object-cover shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] md:h-44 lg:h-60"
-                        />
-                        <img
-                            src='/placeholder.svg'
-                            alt="feature template"
-                            width={500}
-                            height={500}
-                            className="h-20 w-full rounded-lg object-cover shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] md:h-44 lg:h-60"
-                        />
-                        <img
-                            src='/placeholder.svg'
                             width={500}
                             height={500}
                             className="h-20 w-full rounded-lg object-cover shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] md:h-44 lg:h-60"
@@ -184,53 +149,94 @@ export function TimelineDemo() {
         {
             title: "2024",
             content: (
-                <div>
+                <div >
                     <p className="mb-8 text-xs font-normal md:text-sm bg-gradient-to-r from-purple-400 to-pink-400 bg-clip-text text-transparent">
                         Aprendi novas ferramentas de desenvolvimento como <span className="italic">NextJS</span>, <span className="italic">React</span> e <span className="italic">Tailwind CSS</span>. Também busquei conhecimento sobre SEO para melhor ranking do site.
                     </p>
                     <p className="mb-8 text-xs font-normal md:text-sm bg-gradient-to-r from-purple-400 to-pink-400 bg-clip-text text-transparent">
                         Realizei projetos de analise de dados e de desenvolvimento de web Aplications. Você pode dar uma olhada na barra superior à página na opção &quot;Projetos&quot;. Começei a usar UI das plataformar aceternety e shadcn.
                     </p>
-                    <div className="mb-8">
-                        <div className="flex gap-2 items-center text-neutral-300 dark:text-neutral-300 text-xs md:text-sm">
-                            ✅ Promptopia
-                        </div>
-                        <div className="flex gap-2 items-center text-neutral-300 dark:text-neutral-300 text-xs md:text-sm">
-                            ✅ Aprendizado de máquina
-                        </div>
-                        <div className="flex gap-2 items-center text-neutral-300 dark:text-neutral-300 text-xs md:text-sm">
-                            ✅ Websites focados em SEO
-                        </div>
-                        <div className="flex gap-2 items-center text-neutral-300 dark:text-neutral-300 text-xs md:text-sm">
-                            ✅ Aceternity & Shadcn UI
-                        </div>
-                        <div className="flex gap-2 items-center text-neutral-300 dark:text-neutral-300 text-xs md:text-sm">
-                            ✅ Design e Boa escrita
-                        </div>
-                    </div>
                     <div className="grid grid-cols-2 gap-4">
                         <img
-                            src='/placeholder.svg'
+                            src='/timeline/devnext1.png'
                             alt="hero template"
                             width={500}
                             height={500}
                             className="h-20 w-full rounded-lg object-cover shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] md:h-44 lg:h-60"
                         />
                         <img
-                            src='/placeholder.svg'
+                            src='/timeline/portfolio2.png'
                             alt="feature template"
                             width={500}
                             height={500}
                             className="h-20 w-full rounded-lg object-cover shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] md:h-44 lg:h-60"
                         />
                         <img
-                            src='/placeholder.svg'
+                            src='/timeline/cs50.png'
+                            alt="feature template"
+                            width={500}
+                            height={500}
+                            className="h-20 w-full rounded-lg object-cover shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] md:h-44 lg:h-60"
+                        />
+
+
+                    </div>
+                </div>
+            ),
+        },
+        {
+            title: "Atualmente - Futuro",
+            content: (
+                <div>
+                    <p className="mb-8 text-xs font-normal md:text-sm bg-gradient-to-r from-purple-400 to-pink-400 bg-clip-text text-transparent">
+                        Em 2025 Percebo uma demanda adormecida nas cidades de Camocim de São félix e região, pretendo ser pioneiro e ajudar as empresas locais e governamentais a se digitalizarem. Evitando a "Fulga de cérebro" que acontece com frequência na cidade. Criando profissionais de TI e de análise de dados.
+                    </p>
+                    <p className="mb-8 text-xs font-normal md:text-sm bg-gradient-to-r from-purple-400 to-pink-400 bg-clip-text text-transparent">
+                        Minha visão de futuro é criar uma empresa de desenvolvimento de software e de análise de dados. E fazer Camocim ser um polo tecnológico na região e ter uma imagem de exelência bem como um polo de inovação. Garantindo segurança cibernética e otimizando processos sociais com a ajuda de ferramentas tecnológicas.
+                    </p>
+                    <div className="mb-8">
+                        <h2 className="text-sm text-neutral-400 uppercase mb-2">🎯 Metas de 2025</h2>
+
+                        <div className="flex gap-2 items-center text-neutral-300 dark:text-neutral-300 text-xs md:text-sm">
+                            ✅ Sistema de Gerenciamento com autenticação e banco de dados
+                        </div>
+                        <div className="flex gap-2 items-center text-neutral-300 dark:text-neutral-300 text-xs md:text-sm">
+                            ✅ Atualizar meu portifólio para uma versão mais limpa
+                        </div>
+                        <div className="flex gap-2 items-center text-neutral-300 dark:text-neutral-300 text-xs md:text-sm">
+                            ⏳ Iniciar processo para fomentar o desenvolvimento de software na região
+                        </div>
+                        <div className="flex gap-2 items-center text-neutral-300 dark:text-neutral-300 text-xs md:text-sm">
+                            ⏳ Criar um sistema de criação de formulários para gestão empresarial
+                        </div>
+                        <div className="flex gap-2 items-center text-neutral-300 dark:text-neutral-300 text-xs md:text-sm">
+                            ⏳ Lançar uma plataforma SaaS voltada para micro e pequenas empresas (Lojas, Restaurantes, etc)
+                        </div>
+                    </div>
+
+                    <div className="grid grid-cols-2 gap-4">
+                        <img
+                            src='/timeline/gym4.png'
+                            alt="hero template"
                             width={500}
                             height={500}
                             className="h-20 w-full rounded-lg object-cover shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] md:h-44 lg:h-60"
                         />
                         <img
-                            src='/placeholder.svg'
+                            src='/timeline/gym3.png'
+                            alt="feature template"
+                            width={500}
+                            height={500}
+                            className="h-20 w-full rounded-lg object-cover shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] md:h-44 lg:h-60"
+                        />
+                        <img
+                            src='/timeline/gym2.png'
+                            width={500}
+                            height={500}
+                            className="h-20 w-full rounded-lg object-cover shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] md:h-44 lg:h-60"
+                        />
+                        <img
+                            src='/timeline/gym1.png'
                             alt="cards template"
                             width={500}
                             height={500}
